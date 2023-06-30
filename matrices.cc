fea504c75aca594e6e8854f5f50452cc764219ec
Isaac Barker <isaacbarkerdesign@hotmail.com>
2023-06-30 19:20:21 +0100

    Added print function for matrix testing and examples

--- matrices.cc
+++ matrices.cc
@@ -11,14 +11,10 @@ int main() {
 
   // Multiplication
   matrix_1.apply(matrix_2);
-  matrix_1.print();
-
   matrix_1.apply(3);
-  matrix_1.print();
 
   // Addition
   matrix_2.add(matrix_1);
-  matrix_2.print();
   
   // Subtraction
   matrix_2.subtract(matrix_1);
